839a2ae0ed420bea189b3ddb90ca0683278bf7fd
Manvir Singh <manvir.singh@walkwel.in>
2018-05-24 12:40:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+    '<div *ngIf="myObject then Mytemp1 else  Mytemp2"></div><ng-template #Mytemp1>I am actually true</ng-template><ng-template #Mytemp2>I am actually false</ng-template>',
+
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+  myObject = false;
 }
